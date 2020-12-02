1bdb49b34d7bcf3b05a9c215a0714c5ff802be4e
biao.li <biao.li@telink-semi.com>
2020-12-02 14:28:54 +0800

    merge sdk.

--- eagle_ble_sdk/stack/ble/host/host_stack.h
+++ eagle_ble_sdk/stack/ble/host/host_stack.h
@@ -47,51 +47,6 @@
 #ifndef STACK_BLE_HOST_HOST_STACK_H_
 #define STACK_BLE_HOST_HOST_STACK_H_
 
-#include "tl_common.h"
-#include "drivers.h"
-
-
-
-
-_attribute_aligned_(4)
-typedef struct {
-	u16 	l2cap_connParaUpdateReq_minIntevel;
-	u16 	l2cap_connParaUpdateReq_maxInterval;
-	u16 	l2cap_connParaUpdateReq_latency;
-	u16 	l2cap_connParaUpdateReq_timeout;
-
-	u32		l2cap_connParaUpReq_pending;  //must "u32"
-//	u8		u8_rsvd[3];
-
-
-
-
-} host_acl_ms_t;
-extern host_acl_ms_t blhAclms[];
-
-_attribute_aligned_(4)
-typedef struct {
-	u32	rsvd;
-
-} host_acl_m_t;
-extern host_acl_m_t blhAclm[];
-
-
-_attribute_aligned_(4)
-typedef struct {
-	u32	rsvd;
-
-} host_acl_s_t;
-extern host_acl_s_t blhAcls[];
-
-
-
-
-
-
-
-
-
 
 
 
