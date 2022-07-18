375bca9a7bda67781298735f1460a90611ee0a8c
wei.sun <wei.sun@telink-semi.com>
2022-07-18 10:26:02 +0800

    [update license]

--- B91_ble_sdk/vendor/B91_feature/feature_ir/rc_ir_learn.h
+++ B91_ble_sdk/vendor/B91_feature/feature_ir/rc_ir_learn.h
@@ -1,48 +1,26 @@
 /********************************************************************************************************
- * @file	rc_ir_learn.h
+ * @file     rc_ir_learn.h
  *
- * @brief	This is the header file for BLE SDK
+ * @brief    This is the header file for BLE SDK
  *
- * @author	BLE GROUP
- * @date	2020.06
+ * @author	 BLE GROUP
+ * @date         06,2022
  *
- * @par     Copyright (c) 2020, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
- *          All rights reserved.
+ * @par     Copyright (c) 2022, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
  *
- *          Redistribution and use in source and binary forms, with or without
- *          modification, are permitted provided that the following conditions are met:
+ *          Licensed under the Apache License, Version 2.0 (the "License");
+ *          you may not use this file except in compliance with the License.
+ *          You may obtain a copy of the License at
  *
- *              1. Redistributions of source code must retain the above copyright
- *              notice, this list of conditions and the following disclaimer.
+ *              http://www.apache.org/licenses/LICENSE-2.0
  *
- *              2. Unless for usage inside a TELINK integrated circuit, redistributions
- *              in binary form must reproduce the above copyright notice, this list of
- *              conditions and the following disclaimer in the documentation and/or other
- *              materials provided with the distribution.
- *
- *              3. Neither the name of TELINK, nor the names of its contributors may be
- *              used to endorse or promote products derived from this software without
- *              specific prior written permission.
- *
- *              4. This software, with or without modification, must only be used with a
- *              TELINK integrated circuit. All other usages are subject to written permission
- *              from TELINK and different commercial license may apply.
- *
- *              5. Licensee shall be solely responsible for any claim to the extent arising out of or
- *              relating to such deletion(s), modification(s) or alteration(s).
- *
- *          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- *          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- *          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *          DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
- *          DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- *          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- *          ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- *          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ *          Unless required by applicable law or agreed to in writing, software
+ *          distributed under the License is distributed on an "AS IS" BASIS,
+ *          WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *          See the License for the specific language governing permissions and
+ *          limitations under the License.
  *******************************************************************************************************/
+
 #ifndef RC_IR_LEARN_H_
 #define RC_IR_LEARN_H_
 #if (FEATURE_TEST_MODE == TEST_IR)
@@ -52,10 +30,10 @@
 #define  GPIO_IR_LEARN_IN	                 GPIO_PE1
 
 /************************************************************************************************
-* ¡¾define¡¿IR_LEARN_FREQUENCY_RANGE_MODE :														*
+* defineIR_LEARN_FREQUENCY_RANGE_MODE :														*
 * Choose IR Learn frequency range that device supported											*
-* ¡¾define¡¿IR_LEARN_MAX_FREQUENCY: Max carrier frequency that device support to do IR learn		*
-* ¡¾define¡¿IR_LEARN_MIN_FREQUENCY: Min carrier frequency that device support to do IR learn		*
+* defineIR_LEARN_MAX_FREQUENCY: Max carrier frequency that device support to do IR learn		*
+* defineIR_LEARN_MIN_FREQUENCY: Min carrier frequency that device support to do IR learn		*
 ************************************************************************************************/
 
 #define			IR_LEARN_MAX_FREQUENCY			40000
@@ -67,7 +45,7 @@
 #define			IR_LEARN_CARRIER_MAX_HIGH_TICK	IR_LEARN_CARRIER_MAX_CYCLE/3
 
 /************************************************************************************************
-* ¡¾define¡¿PARAMETER SETTING :																	*
+* definePARAMETER SETTING :																	*
 * Related parameter setting to change IR learn result											*
 ************************************************************************************************/
 
